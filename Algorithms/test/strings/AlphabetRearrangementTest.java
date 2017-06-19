5caf44db2c6175b4653d8703c79a3012ffa35d72
abhay <abhaybhegde@gmail.com>
2017-06-19 23:18:58 +0530

    Adding JUnit

--- Algorithms/test/strings/AlphabetRearrangementTest.java
+++ Algorithms/test/strings/AlphabetRearrangementTest.java
@@ -20,4 +20,15 @@ public class AlphabetRearrangementTest {
 		AlphabetRearrangement.rearrangeAlphabets("", "");
 		
 	}
+	
+	@Test
+	public void rearrangeAlphabetsShouldReturnElementsWithSpecifiedOrderForLargeInput(){
+		
+		assertEquals("[c, a, z, d, j, m, o, w, e, r, t, u, i, p, s, f, g, h, q]", AlphabetRearrangement.rearrangeAlphabets("a z d e c j m o p w q e r t u i p s f g h k", "3 4 5 2 1 6 7 8 9 10 11"
+				+ "12 13 14 15 16 17 18 19 20 21 22").toString() );
+		
+	}
+
+
+
 }
