024318f07d30dd6f31f0ad01eec392da60fe9b4a
abhay <abhaybhegde@gmail.com>
2017-07-20 12:13:34 +0530

    Added LeftSkewed JUnit

--- Algorithms/test/trees/SearchKeyInBSTTest.java
+++ Algorithms/test/trees/SearchKeyInBSTTest.java
@@ -47,6 +47,16 @@ public class SearchKeyInBSTTest {
 		root.rightChild.rightChild.rightChild = new Node(40);
 		assertEquals(40, SearchKeyInBST.searchKeyInBST(root,40).data);		
 	}
+	
+	@Test
+	public void test_with_left_skewed_tree() {
+		
+		Node root = new Node(24);
+		root.leftChild = new Node(20);
+		root.leftChild.leftChild = new Node(15);
+		root.leftChild.leftChild.leftChild = new Node(12);
+		assertEquals(12, SearchKeyInBST.searchKeyInBST(root,12).data);		
+	}
 
 
 }
