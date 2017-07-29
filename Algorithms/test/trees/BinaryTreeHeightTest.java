40028a410d1334c70bf7d962de200c2af0eb4f11
abhay <abhaybhegde@gmail.com>
2017-07-29 16:53:04 +0530

    JUnit+1

--- Algorithms/test/trees/BinaryTreeHeightTest.java
+++ Algorithms/test/trees/BinaryTreeHeightTest.java
@@ -10,5 +10,16 @@ public class BinaryTreeHeightTest {
 	public void test_with_null() {
 		assertEquals(0,BinaryTreeHeight.returnHeightOfBinaryTree(null));
 	}
+	
+	@Test
+	public void test_with_minimal_nodes() {
+		
+		Node root = new Node(10);
+		root.leftChild = new Node(5);
+		root.rightChild = new Node(20);
+		
+		assertEquals(2,BinaryTreeHeight.returnHeightOfBinaryTree(root) );
+		
+	}
 
 }
